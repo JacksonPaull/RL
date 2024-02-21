6279b49bf9b8abcf68789b0ffd80a09b566c36f5
Jackson Paull <jackson@planetpaull.com>
2024-02-20 21:36:33 -0600

    Monte Carlo accepted

--- homeworks/hw2/monte_carlo.py
+++ homeworks/hw2/monte_carlo.py
@@ -23,11 +23,23 @@ def off_policy_mc_prediction_ordinary_importance_sampling(
         Q: $q_pi$ function; numpy array shape of [nS,nA]
     """
 
-    #####################
-    # TODO: Implement Off Policy Monte-Carlo prediction algorithm using ordinary importance
-    # sampling (Hint: Sutton Book p. 109, every-visit implementation is fine)
-    #####################
+    Q = np.copy(initQ)
+    tao = np.zeros(Q.shape)
 
+    for traj in trajs:
+        G = 0
+        W = 1
+
+        # Loop from t = T -> 0
+        for t in np.arange(len(traj))[::-1]:
+            s_t, a_t, r_t1, s_t1 = traj[t]
+            tao[s_t, a_t] += 1
+            G = env_spec.gamma * G + r_t1
+            Q[s_t, a_t] = Q[s_t, a_t] + W / tao[s_t, a_t] * (G - Q[s_t, a_t]) 
+            W = W * (pi.action_prob(s_t, a_t)) / (bpi.action_prob(s_t, a_t))
+
+            if W == 0:
+                break
     return Q
 
 def off_policy_mc_prediction_weighted_importance_sampling(
@@ -49,9 +61,21 @@ def off_policy_mc_prediction_weighted_importance_sampling(
         Q: $q_pi$ function; numpy array shape of [nS,nA]
     """
 
-    #####################
-    # TODO: Implement Off Policy Monte-Carlo prediction algorithm using weighted importance
-    # sampling (Hint: Sutton Book p. 110, every-visit implementation is fine)
-    #####################
+    Q = np.copy(initQ)
+    C = np.zeros(Q.shape)
 
+    for traj in trajs:
+        G = 0
+        W = 1
+
+        # Loop from t = T -> 0
+        for t in np.arange(len(traj))[::-1]:
+            s_t, a_t, r_t1, s_t1 = traj[t]
+            G = env_spec.gamma * G + r_t1
+            C[s_t, a_t] = C[s_t, a_t] + W
+            Q[s_t, a_t] = Q[s_t, a_t] + W / C[s_t, a_t] * (G - Q[s_t, a_t])
+            W = W * (pi.action_prob(s_t, a_t)) / (bpi.action_prob(s_t, a_t))
+
+            if W == 0:
+                break
     return Q
